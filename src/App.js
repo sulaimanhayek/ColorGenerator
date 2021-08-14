60bf9503fb3d0338b64b0a71c931b419a9bdf12e
Sulaiman <suleiman_hayek@yahoo.com>
2021-08-14 05:26:45 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [color,setColor] = useState("");
+  const [error,setError] = useState(false);
+  const [list,setList] = useState([]);
+  
+  return <h2>color generator project</h2>
 }
 
-export default App;
+export default App
